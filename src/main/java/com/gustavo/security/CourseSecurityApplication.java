9b0155039a53fb361fedc4740e0d155a1f6efda3
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-06-03 14:54:49 -0300

    Initial strucure has been done

---
+++ src/main/java/com/gustavo/security/CourseSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.gustavo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CourseSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSecurityApplication.class, args);
+	}
+
+}
