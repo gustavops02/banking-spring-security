1cb44c2830e5535b06ea518924b7975338fc7007
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-15 00:40:12 -0300

    Auth Provider has been implemented itself

---
+++ src/main/java/com/gustavo/security/config/SecurityUsernamePasswordAuthProvider.java
@@ -0,0 +1,53 @@
+package com.gustavo.security.config;
+
+import com.gustavo.security.entities.Customer;
+import com.gustavo.security.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SecurityUsernamePasswordAuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        Customer customer = customerRepository.findByEmail(email);
+
+        if (customer != null) {
+            if(passwordEncoder.matches(password, customer.getPassword())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+                return new UsernamePasswordAuthenticationToken(email, password, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password");
+            }
+        } else {
+            throw new BadCredentialsException("No user registered with this email.");
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
