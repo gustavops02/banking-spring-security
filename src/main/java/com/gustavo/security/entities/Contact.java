e5bafbfb3c99eae52e99cb3f6eee6a047154a2a4
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 15:46:41 -0300

    Create Contact entity class

---
+++ src/main/java/com/gustavo/security/entities/Contact.java
@@ -0,0 +1,102 @@
+package com.gustavo.security.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "contact_messages")
+public class Contact {
+
+    @Column(name = "contact_id")
+    private String contactId;
+
+    @Column(name = "contact_name")
+    private String contactName;
+
+    @Column(name = "contact_email")
+    private String contactEmail;
+
+    private String subject;
+    private String message;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Contact(String contactId, String contactName, String contactEmail, String subject, String message, Date createdAt) {
+        this.contactId = contactId;
+        this.contactName = contactName;
+        this.contactEmail = contactEmail;
+        this.subject = subject;
+        this.message = message;
+        this.createdAt = createdAt;
+    }
+
+    public Contact() {
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(contactId, contact.contactId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contactId);
+    }
+}
