1cc2b5ffb9fd1b14121cc191b013d1882482189d
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 16:22:39 -0300

    Create all *Repository interfaces

--- src/main/java/com/gustavo/security/repositories/CustomerRepository.java
+++ src/main/java/com/gustavo/security/repositories/CustomerRepository.java
@@ -4,7 +4,6 @@ import com.gustavo.security.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
