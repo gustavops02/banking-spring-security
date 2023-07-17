ea9fa49591983f1e649854d231516ea6d376f2aa
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 15:23:13 -0300

    Create the Accounts entity class

---
+++ src/main/java/com/gustavo/security/entities/Accounts.java
@@ -0,0 +1,94 @@
+package com.gustavo.security.entities;
+
+
+import com.gustavo.security.controllers.AccountController;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "accounts")
+public class Accounts {
+
+    @Id
+    @Column(name = "account_number")
+    private long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAdress;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "customer_id")
+    private int customerId;
+
+    public Accounts() {}
+
+    public Accounts(long accountNumber, String accountType, String branchAdress, String createdAt, int customerId) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.branchAdress = branchAdress;
+        this.createdAt = createdAt;
+        this.customerId = customerId;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBranchAdress() {
+        return branchAdress;
+    }
+
+    public void setBranchAdress(String branchAdress) {
+        this.branchAdress = branchAdress;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Accounts accounts = (Accounts) o;
+        return accountNumber == accounts.accountNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber);
+    }
+}
