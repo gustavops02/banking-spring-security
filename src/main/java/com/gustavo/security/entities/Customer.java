1b38e8a5a76d76ea098371dc1ed911cd57dca31a
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 15:51:32 -0300

    Update the Customer.java entity class

--- src/main/java/com/gustavo/security/entities/Customer.java
+++ src/main/java/com/gustavo/security/entities/Customer.java
@@ -2,19 +2,45 @@ package com.gustavo.security.entities;
 
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+import java.util.Objects;
 
 
 @Entity
-@Table(name = "customer")
+
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name= "native")
+    @Column(name = "customer_id")
     private Integer id;
 
+    private String name;
     private String email;
-    private String password;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    private String pwd;
     private String role;
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String email, String mobileNumber, String pwd, String role, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.pwd = pwd;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
 
     public Integer getId() {
         return id;
@@ -22,6 +48,14 @@ public class Customer {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -32,12 +66,20 @@ public class Customer {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
+    public String getMobileNumber() {
+        return mobileNumber;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
     }
 
     public String getRole() {
@@ -46,5 +88,26 @@ public class Customer {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
