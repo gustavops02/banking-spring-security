053f8bd4bd3814fb4d912195a769f0b96bd9d208
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 16:34:20 -0300

    Create the getBalanceDetails method for BalanceController

--- src/main/java/com/gustavo/security/controllers/BalanceController.java
+++ src/main/java/com/gustavo/security/controllers/BalanceController.java
@@ -1,15 +1,30 @@
 package com.gustavo.security.controllers;
 
+import com.gustavo.security.entities.Transactions;
+import com.gustavo.security.repositories.TransactionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/balance")
 public class BalanceController {
 
+    @Autowired
+    private TransactionsRepository transactionsRepository;
+
     @GetMapping
-    public String getBalanceDetails() {
-        return "Balance details";
+    public List<Transactions> getBalanceDetails(@RequestParam int id) {
+
+        List<Transactions> transactions = transactionsRepository.findByCustomerIdOrderByTransactionDateDesc(id);
+
+        if (transactions != null) {
+            return transactions;
+        }
+        else return null;
     }
 }
