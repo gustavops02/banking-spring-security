9bbe3cd57ce37b9a1570ad3d139403a31e6bc291
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-07-17 16:29:36 -0300

    Create the getAccountDetails get method for AccountController

--- src/main/java/com/gustavo/security/controllers/AccountController.java
+++ src/main/java/com/gustavo/security/controllers/AccountController.java
@@ -1,17 +1,30 @@
 package com.gustavo.security.controllers;
 
 
+import com.gustavo.security.entities.Accounts;
+import com.gustavo.security.repositories.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/account")
 public class AccountController {
 
-    @GetMapping
-    public String getAccountDetails() {
-        return "Here are the account.";
+    @Autowired
+    public AccountsRepository accountsRepository;
+
+    @GetMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts account = accountsRepository.findByCustomerId(id);
+
+        if(account != null) {
+            return account;
+        } else {
+            return null;
+        }
     }
 
 }
